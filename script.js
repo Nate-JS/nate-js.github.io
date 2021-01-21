da70c4f80962f6cd9d3b7e8cd5d67f97e5e5e579
Nathan Archer <nxtx.dev@gmail.com>
2021-01-21 21:07:12 +0800

    Update

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("everything works... ")
+"use strict";
+
+let canvas, context, snake;
+
+const BLOCK_SIZE = 25,
+  SNAKE_COLOR = "blue";
+
+function init() {
+  canvas = document.querySelector("canvas");
+  context = canvas.getContext("2d");
+
+  canvas.style.width = "100%";
+  canvas.style.height = "100%";
+
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+
+  snake = {
+    body: [{ x: canvas.width / 2, y: canvas.height / 2 }],
+    length: 1,
+
+    draw() {
+      this.body.forEach((block) => {
+        context.fillStyle = SNAKE_COLOR;
+        context.strokeStyle = SNAKE_COLOR;
+        context.fillRect(block.x, block.y, BLOCK_SIZE, BLOCK_SIZE);
+      });
+    },
+  };
+}
+
+function update() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  snake.draw();
+
+  requestAnimationFrame(update);
+}
+
+init();
+update();
